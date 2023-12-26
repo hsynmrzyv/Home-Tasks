22028ecd4f17edb1c700d2d8ddf0125895efa0e0
HSYN <huseyn.mirzayev@code.edu.az>
2023-12-26 17:46:19 +0400

    home tasks finished :)

---
+++ Scroll Animation/script.js
@@ -0,0 +1,3 @@
+"use strict";
+
+const animationItems = document.querySelectorAll(".animationItem");
---
+++ Tabs/script.js
@@ -0,0 +1,5 @@
+"use strict";
+
+const tabsContainer = document.querySelector(".tabs-container");
+const tabs = document.querySelectorAll(".tab");
+const tabContents = document.querySelectorAll(".tab__content");
